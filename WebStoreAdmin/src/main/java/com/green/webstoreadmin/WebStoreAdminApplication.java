1caaf562fd411da5391784660b0c01db99698722
Vo Thanh Cong <thanhcong4135@gmail.com>
2021-07-06 12:05:44 +0700

    Online Bike Store
    Create project

---
+++ WebStoreAdmin/src/main/java/com/green/webstoreadmin/WebStoreAdminApplication.java
@@ -0,0 +1,13 @@
+package com.green.webstoreadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreAdminApplication.class, args);
+	}
+
+}
---
+++ WebStoreAdmin/src/test/java/com/green/webstoreadmin/WebStoreAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.webstoreadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
